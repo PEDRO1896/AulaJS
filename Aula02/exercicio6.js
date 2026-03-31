ac7afd41f34ab54b0b2efb4f4e855cd68762456f
PEDRO1896 <pedro.18107118@gmail.com>
2026-03-31 11:38:52 -0300

    31/03/2026
    31/03/2026

---
+++ Aula02/exercicio6.js
@@ -0,0 +1,11 @@
+function Exemplo6()
+{
+
+    letnumero = Number(document.getElementById("numero").value);
+
+    letresultado; 
+    
+   resultado = (lado* lado);
+
+    document.getElementById("resultado").innerHTML="<p>O tamanho do lado da area do quadrado é de: " + parcela +"</p>";
+}
